e9857efdf70f31d0978215cbddfc20100fa894d2
MarionFoto <marionacp@gmail.com>
2017-07-14 02:48:34 -0400

    menu con links

--- js/main2.js
+++ js/main2.js
@@ -10,6 +10,23 @@ $(document).ready(function(){
 			e.preventDefault();
 			window.location = "index3.html";
 		});
+//linkea el menu
+	$("#uneperfil").click(function(e){
+			e.preventDefault();
+			window.location = "index3.html";
+		});
+
+	$("#preguntas").click(function(e){
+			e.preventDefault();
+			window.location = "index4.html";
+		});
+//link desde el menu
+	$("#press").click(function(e){
+			e.preventDefault();
+			window.location = "index4.html";
+		});
+
+
 //funcion para imprimir tarjetas
 	$("#agregar").click(function(e){
 		localStorage.tarjetas = $("#tarjetas").val();
