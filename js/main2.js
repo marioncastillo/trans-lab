683819816fb44b672b0efd7e240222a3bff2c788
MarionFoto <marionacp@gmail.com>
2017-07-14 01:55:07 -0400

    imprime las tarjetas

--- js/main2.js
+++ js/main2.js
@@ -2,10 +2,15 @@
 $(document).ready(function(){
 
 	$("#home").click(function(e){
-
-
 			e.preventDefault();
 			window.location = "index2.html";
-		}
-	);
+		});
+
+//funcion para imprimir tarjetas
+	$("#agregar").click(function(e){
+		localStorage.tarjetas = $("#tarjetas").val();
+
+	$("#guardatarjetas").append('<p class="imprime">'+localStorage.tarjetas+'</p>');
+	});
+
 });
